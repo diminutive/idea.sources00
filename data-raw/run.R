3a1d4e0ecb139950abdcf1e27c08a7da10f563e6
Michael Sumner <mdsumner@gmail.com>
2023-07-26 10:52:49 +1000

    root

--- data-raw/run.R
+++ data-raw/run.R
@@ -1,10 +1,10 @@
 library(bowerbird)
-library(bowerbird)
-datadir <- "/bower"
+datadir <- tempdir()
 cf <- bb_config(local_file_root = datadir)
 
 my_source <- bb_example_sources("Australian Election 2016 House of Representatives data")
-
+# blueant::
+# amsr_source <- bb_example_sources("Artist AMSR2 near-real-time 3.125km sea ice concentration")
 ## add this data source to the configuration
 cf <- bb_add(cf, my_source)
 

f15bc66be20c6ae84fbbf5e0a4dfbfff6bff2cca
Michael Sumner <mdsumner@gmail.com>
2023-07-26 12:03:11 +1000

    e

--- data-raw/run.R
+++ data-raw/run.R
@@ -1,20 +1,21 @@
 library(bowerbird)
-datadir <- "/bower"
+datadir <- file.path(tempdir(), "bower") #"/bower"
 cf <- bb_config(local_file_root = datadir)
 
 dates <- Sys.Date() - (10:0)
 dates <- format(dates, "%Y%m%d")
 
 x <- bb_example_sources("Australian Election 2016 House of Representatives data")
-#x<- blueant::sources("Artist AMSR2 near-real-time 3.125km sea ice concentration")
-#year <- as.character(as.integer(format(Sys.Date(), "%Y")) + c(0))
-#x$source_url[[1]] <- paste0(fs::path(x$source_url[[1]], year), "/")
+x<- blueant::sources("Artist AMSR2 near-real-time 3.125km sea ice concentration")
+year <- as.character(as.integer(format(Sys.Date(), "%Y")) + c(0))
+x$source_url[[1]] <- paste0(fs::path(x$source_url[[1]], year), "/")
+x$name <- "(RECENT ONLY) Artist AMSR2 near-real-time 3.125km sea ice concentration"
 
-#x$method[[1]]$accept_download <- sprintf("Antarctic3125/asi.*%s.*", dates[1])
+#x$method[[1]]$accept_download <- "Antarctic3125/asi.*20230701.*\\.(hdf|png|tif)"
 
 #x$method[[1]]$accept_download <- c(x$method[[1]]$accept_download, dates[1])
 
-#x$collection_size <- 0.01
+x$collection_size <- 0.01
 ## add this data source to the configuration
 cf <- bb_add(cf, x)
 

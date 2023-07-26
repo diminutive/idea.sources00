7d1b29a0a9f5e1bec99f3e9c4c246d88839bd02a
Michael Sumner <mdsumner@gmail.com>
2023-07-26 11:43:55 +1000

    can only try

--- data-raw/run.R
+++ data-raw/run.R
@@ -1,11 +1,21 @@
 library(bowerbird)
-datadir <- tempdir()
+datadir <- "/bower"
 cf <- bb_config(local_file_root = datadir)
 
-my_source <- bb_example_sources("Australian Election 2016 House of Representatives data")
-# blueant::
-# amsr_source <- bb_example_sources("Artist AMSR2 near-real-time 3.125km sea ice concentration")
+dates <- Sys.Date() - (10:0)
+dates <- format(dates, "%Y%m%d")
+
+x <- bb_example_sources("Australian Election 2016 House of Representatives data")
+#x<- blueant::sources("Artist AMSR2 near-real-time 3.125km sea ice concentration")
+#year <- as.character(as.integer(format(Sys.Date(), "%Y")) + c(0))
+#x$source_url[[1]] <- paste0(fs::path(x$source_url[[1]], year), "/")
+
+#x$method[[1]]$accept_download <- sprintf("Antarctic3125/asi.*%s.*", dates[1])
+
+#x$method[[1]]$accept_download <- c(x$method[[1]]$accept_download, dates[1])
+
+#x$collection_size <- 0.01
 ## add this data source to the configuration
-cf <- bb_add(cf, my_source)
+cf <- bb_add(cf, x)
 
 status <- bb_sync(cf, verbose = TRUE, create_root = TRUE)

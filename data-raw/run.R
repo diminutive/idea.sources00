c9f01195e5aa105ad8db6d1e2f010931a9908921
Michael Sumner <mdsumner@gmail.com>
2023-07-26 10:45:55 +1000

    do stuff

--- data-raw/run.R
+++ data-raw/run.R
@@ -1,1 +1,11 @@
 library(bowerbird)
+library(bowerbird)
+datadir <- "/bower"
+cf <- bb_config(local_file_root = datadir)
+
+my_source <- bb_example_sources("Australian Election 2016 House of Representatives data")
+
+## add this data source to the configuration
+cf <- bb_add(cf, my_source)
+
+status <- bb_sync(cf, verbose = TRUE, create_root = TRUE)

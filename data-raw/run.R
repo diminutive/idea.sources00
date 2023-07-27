d1b2ce663151de1b8e5746374befc17098d89a45
Michael Sumner <mdsumner@gmail.com>
2023-07-27 11:30:11 +1000

    avoid old v5

--- data-raw/run.R
+++ data-raw/run.R
@@ -3,7 +3,7 @@ datadir <- normalizePath(file.path("~", "bower_dir"), mustWork = FALSE) #"/bower
 if (!file.exists(datadir)) dir.create(datadir)
 cf <- bb_config(local_file_root = datadir)
 
-dates <- Sys.Date() - (10:0)
+dates <- Sys.Date() - (5:0)
 datesuffix <- file.path(unique(tolower(format(dates, "%Y/%b"))), "Antarctic3125")
 
 
@@ -22,6 +22,7 @@ x$method[[1]]$level <- 3
 x$method[[1]]$accept_download <- "Antarctic3125/asi.*\\.(tif|hdf)$"
 ## can we do an vector OR on dates above
 x$method[[1]]$accept_download <- sprintf("Antarctic3125/asi.*(%s).*\\.(tif|hdf)$", paste0(format(dates, "%Y%m%d"), collapse = "|"))
+x$method[[1]]$reject_download <- ".*v5.(tif|hdf)"  ## save copying the symlink v5 files
 
 x$collection_size <- 0.01
 ## add this data source to the configuration
@@ -29,4 +30,6 @@ cf <- bb_add(cf, x)
 status <- bb_sync(cf, verbose = FALSE)
 
 print(status$files)
+
+#date <- as.Date(strptime(stringr::str_extract(basename(tail(status$files[[1]]$file, 1)), "[0-9]{8}"), "%Y%m%d"))
 
